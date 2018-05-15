4dd21fbe9846f07c6e545f421d3bdc74f00051b6
Anthony Samartsev <ant.samartsev@gmail.com>
2018-05-15 08:48:05 +0300

    init, v. 1.0.0

---
+++ app/src/main/java/com/github/kycko/kyckorandomizer/IntRandomizerResultActivity.java
@@ -0,0 +1,75 @@
+package com.github.kycko.kyckorandomizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class IntRandomizerResultActivity extends AppCompatActivity {
+    private final Random rnd = new Random();
+    private cubeVariations cubeVariations;
+    private int max;
+    private String activityType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_int_randomizer_result);
+
+        // Toolbar back button
+        Toolbar toolbarObj = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbarObj);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        activityType = getIntent().getStringExtra(MainActivity.intentActivityType);
+        switch (activityType) {
+            case "int":
+                final String myString = getIntent().getStringExtra(MainActivity.intentMaxNum);
+                max = Integer.parseInt(myString);
+                toolbarObj.setTitle(getString(R.string.title_diapasone, myString));
+                break;
+            case "cube":
+                cubeVariations = new cubeVariations();
+                max = cubeVariations.getLength();
+                TextView textViewObj = findViewById(R.id.lbl_result);
+                textViewObj.setTextSize(30);
+        }
+        getNewNum();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
+    }
+    private void getNewNum() {
+        Integer cur;
+        String toDisplay = "";
+        final TextView txtViewObj = findViewById(R.id.lbl_result);
+
+        switch (activityType) {
+            case "int":
+                do cur = rnd.nextInt(max)+1;
+                while (cur == Integer.parseInt(txtViewObj.getText().toString()));
+                toDisplay = cur.toString();
+                break;
+            case "cube":
+                toDisplay = cubeVariations.getNew(rnd);
+        }
+        txtViewObj.setText(toDisplay);
+    }
+    public void buttonIntRandomizerResultRefreshPressed(View view) {
+        getNewNum();
+        final Animation anim = AnimationUtils.loadAnimation(this, R.anim.abc_grow_fade_in_from_bottom);
+        anim.setDuration(500);
+        findViewById(R.id.lbl_result).startAnimation(anim);
+    }
+}
---
+++ app/src/main/java/com/github/kycko/kyckorandomizer/MainActivity.java
@@ -0,0 +1,70 @@
+package com.github.kycko.kyckorandomizer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String intentMaxNum       = "com.github.kycko.kyckorandomizer.max_num";
+    public static final String intentActivityType = "com.github.kycko.kyckorandomizer.activityType";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+    // menu (toolbar right cube)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.run_cube:
+                launchIntAndCubeRandomizer("cube");
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+    // my funcs
+    private void launchIntAndCubeRandomizer(String myString) {
+        final Intent intent = new Intent(this, IntRandomizerResultActivity.class);
+        String typeStringObj = new String("cube");
+        if (!myString.equals(typeStringObj)) {
+            intent.putExtra(intentMaxNum, myString);
+            typeStringObj = new String("int");
+        }
+        intent.putExtra(intentActivityType, typeStringObj);
+        startActivity(intent);
+    }
+    public void buttonGetFromInputPressed(View view) {
+        final EditText reqWidget = findViewById(R.id.input_max_num);
+        final String myString = reqWidget.getText().toString();
+
+        if (!myString.isEmpty() && Integer.parseInt(myString) != 1) launchIntAndCubeRandomizer(myString);
+        else Toast.makeText(
+                getApplicationContext(),
+                getString(R.string.int_randomizer_null_input_toast),
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+    public void button2Pressed (View view) {extractButtonText(R.id.button2);}
+    public void button3Pressed (View view) {extractButtonText(R.id.button3);}
+    public void button10Pressed(View view) {extractButtonText(R.id.button10);}
+
+    private void extractButtonText(int ID) {
+        final Button reqWidget = findViewById(ID);
+        launchIntAndCubeRandomizer(reqWidget.getText().toString());
+    }
+}
